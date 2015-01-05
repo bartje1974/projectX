00efb21c66358b6dfaf228197a91294c5a34ba78
bart <bart@bart-Presario-V4000-EW895EA-ABH>
2015-01-05 22:11:36 +0100

    version 0.01 dev

--- projectx/controllers/home.php
+++ projectx/controllers/home.php
@@ -1,11 +1,38 @@
 <?php
 namespace projectx\controllers;
 use projectx\core\controller;
+use projectx\core\vendor\auth\authentication;
+
 
 class home extends controller
 {
+    protected $auth;
+    protected $message;
+
+
     public function index()
     {
-          $this->view('home');
+        $username = 'bart@cs-hosting.nl';
+        $password = 'kwibus';
+        $this->auth = new authentication;
+        
+        //$data = $this->auth->get_auth('users', $username, $password);
+        
+        //if($data == false)
+        //{
+          //  echo 'Something goes wrong with auth';
+        //}
+        
+        
+        if($this->auth->is_auth() === FALSE)
+        {
+            echo 'Not logged in!';
+        }
+        else
+        {
+            echo 'logged in!';
+        }
+        $this->auth->remove_auth();
+         // $this->view('home');
     }  
 }
