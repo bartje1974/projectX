d8ca1b57690875a1e93feb84a620325ce963eb50
bart <bart@bart-Presario-V4000-EW895EA-ABH>
2015-01-14 08:51:04 +0100

    add some tables

--- projectx/core/vendor/auth/users.sql
+++ projectx/core/vendor/auth/users.sql
@@ -5,6 +5,35 @@ CREATE TABLE IF NOT EXISTS users (  id int(10) NOT NULL AUTO_INCREMENT,
                                     created_at datetime NOT NULL,
                                     last_login datetime NOT NULL,
                                     login_hash varchar(255) COLLATE utf8_bin NOT NULL,
+                                    group_id INT(10) DEFAULT '1',
+                                    permission_id INT(10),
                                     PRIMARY KEY (id),
                                     UNIQUE KEY email (email)
                                   ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
+
+CREATE TABLE IF NOT EXISTS usergroups ( group_id int(10) NOT NULL AUTO_INCREMENT,
+                                        group_name VARCHAR(100) NOT NULL,
+                                        PRIMARY KEY (group_id)
+                                      ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
+
+
+CREATE TABLE IF NOT EXISTS permissions ( pid INT(10) NOT NULL AUTO_INCREMENT,
+                                         permission_name VARCHAR(100) NOT NULL,
+                                         PRIMARY KEY (pid)
+                                       ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
+
+ALTER TABLE
+    users
+ADD
+    FOREIGN KEY(group_id)
+        REFERENCES usergroups (group_id)
+ON DELETE RESTRICT
+ON UPDATE CASCADE
+
+ALTER TABLE
+    users
+ADD
+    FOREIGN KEY(permission_id)
+        REFERENCES user_permission (user_pid)
+ON DELETE RESTRICT
+ON UPDATE CASCADE
